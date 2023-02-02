cc8075afb90cc8c665ac226d02d2a80066890358
oleggorina <oleg.gorina@gmail.com>
2023-02-02 19:04:13 +0200

    Finished blog-modal component & fixed adaptive bugs

--- src/app/Components/card-blog/blog-modal/blog-modal.component.ts
+++ src/app/Components/card-blog/blog-modal/blog-modal.component.ts
@@ -10,6 +10,7 @@ export class BlogModalComponent {
   images: string[] = [this.data.mainImage, this.data.firstImage, this.data.secondImage, 
                       this.data.thirdImage, this.data.fourthImage, this.data.fifthImage];
   currentIndex: number = 0;
+  
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
 
@@ -17,15 +18,7 @@ export class BlogModalComponent {
     return `${this.images[this.currentIndex]}`;
   }
   
-  next(): void {
-    const isLastSlide = this.currentIndex === this.images.length - 1;
-    const newIndex = isLastSlide ? 0 : this.currentIndex + 1;
-    this.currentIndex = newIndex;
-  }
-
-  prev(): void {
-    const isFirstSlide = this.currentIndex === 0;
-    const newIndex = isFirstSlide ? this.images.length - 1 : this.currentIndex - 1;
-    this.currentIndex = newIndex
+  goToSlide(slideIndex: number): void {
+    this.currentIndex = slideIndex;
   }
 }
