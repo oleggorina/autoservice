28c10eca226e74de2b3105c4be8e9fd505cda366
oleggorina <oleg.gorina@gmail.com>
2023-02-21 17:06:34 +0200

    init gallery component

--- src/app/Pages/gallery/gallery.component.ts
+++ src/app/Pages/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import { IGallery } from 'src/app/Shared/gallery.class';
 import { GALLERY_DATA } from 'src/app/Shared/gallery.const';
 
@@ -7,15 +7,15 @@ import { GALLERY_DATA } from 'src/app/Shared/gallery.const';
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
-export class GalleryComponent implements OnInit, AfterViewInit {
+export class GalleryComponent implements OnInit, AfterContentInit {
   images: IGallery[] = GALLERY_DATA;
 
   @ViewChild('slider')
   slider!: ElementRef<HTMLDivElement>;
   @ViewChild('sliderContainer')
   sliderContainer!: ElementRef<HTMLDivElement>;
-  @ViewChild('sliderItem')
-  sliderItem!: QueryList<ElementRef>;
+  @ViewChildren('sliderItems')
+  sliderItems!: QueryList<ElementRef>;
 
   activeOrder: number = 0;
   width: number = 0;
@@ -26,7 +26,30 @@ export class GalleryComponent implements OnInit, AfterViewInit {
     this.width = window.innerWidth;
   }
 
-  ngAfterViewInit(): void {
-    
+  ngAfterContentInit(): void {
+    this.initSlider();
+  }
+
+  initSlider(): void {
+    this.activeOrder = Math.floor(this.sliderItems.length / 2);
+    console.log(this.activeOrder)
+    this.updateSlider();
+  }
+
+  updateSlider(): void {
+    const {width, height} = this.sliderContainer.nativeElement.getBoundingClientRect();
+    const a = width / 2;
+    const b = height / 2;
+    const delta = Math.PI / this.sliderItems.length / 4;
+    for (let i = 0; i < this.sliderItems.length; i ++) {
+      const leftSide = this.renderer.selectRootElement(`[data-id="${this.activeOrder - i}"]`);
+      // const leftSide = document.querySelector(`.slider__item[data-order="${this.activeOrder - i}"]`);
+      if (leftSide) {
+        this.renderer.setStyle(leftSide, 'left', `${width / 2 + a * Math.cos((Math.PI * 3) / 2 - delta * i * 2)}px`);
+        this.renderer.setStyle(leftSide, 'top', `${-b * Math.sin((Math.PI * 3) / 2 - delta * i * 2)}px`);
+        this.renderer.setStyle(leftSide, 'zIndex', `${this.sliderItems.length - 1}`);
+        this.renderer.setStyle(leftSide, 'opacity', `${1 - (2 * i) / this.sliderItems.length}`);
+      }
+    }
   }
 }
