966e91bb0c4bcb9e56faa8105bd5f43e6e17e377
oleggorina <oleg.gorina@gmail.com>
2023-02-23 09:55:13 +0200

    finished gallery slider

--- src/app/Pages/gallery/gallery.component.ts
+++ src/app/Pages/gallery/gallery.component.ts
@@ -7,7 +7,7 @@ import { GALLERY_DATA } from 'src/app/Shared/gallery.const';
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
-export class GalleryComponent implements OnInit, AfterContentInit {
+export class GalleryComponent implements OnInit, AfterViewInit {
   images: IGallery[] = GALLERY_DATA;
 
   @ViewChild('slider')
@@ -26,13 +26,12 @@ export class GalleryComponent implements OnInit, AfterContentInit {
     this.width = window.innerWidth;
   }
 
-  ngAfterContentInit(): void {
+  ngAfterViewInit(): void {
     this.initSlider();
   }
 
   initSlider(): void {
     this.activeOrder = Math.floor(this.sliderItems.length / 2);
-    console.log(this.activeOrder)
     this.updateSlider();
   }
 
@@ -41,15 +40,32 @@ export class GalleryComponent implements OnInit, AfterContentInit {
     const a = width / 2;
     const b = height / 2;
     const delta = Math.PI / this.sliderItems.length / 4;
+
     for (let i = 0; i < this.sliderItems.length; i ++) {
-      const leftSide = this.renderer.selectRootElement(`[data-id="${this.activeOrder - i}"]`);
-      // const leftSide = document.querySelector(`.slider__item[data-order="${this.activeOrder - i}"]`);
+      const leftSide = document.querySelector(`.slider__item[data-order="${this.activeOrder - i}"]`)
       if (leftSide) {
         this.renderer.setStyle(leftSide, 'left', `${width / 2 + a * Math.cos((Math.PI * 3) / 2 - delta * i * 2)}px`);
         this.renderer.setStyle(leftSide, 'top', `${-b * Math.sin((Math.PI * 3) / 2 - delta * i * 2)}px`);
-        this.renderer.setStyle(leftSide, 'zIndex', `${this.sliderItems.length - 1}`);
+        this.renderer.setStyle(leftSide, 'zIndex', `${this.sliderItems.length - i}`);
         this.renderer.setStyle(leftSide, 'opacity', `${1 - (2 * i) / this.sliderItems.length}`);
       }
+      const rightSide = document.querySelector(`.slider__item[data-order="${this.activeOrder + i}"]`)
+      if (rightSide) {
+          this.renderer.setStyle(rightSide, 'left', `${width / 2 + a * Math.cos((Math.PI * 3) / 2 + delta * i * 2)}px`);
+          this.renderer.setStyle(rightSide, 'top', `${-b * Math.sin((Math.PI * 3) / 2 + delta * i * 2)}px`);
+          this.renderer.setStyle(rightSide, 'zIndex', `${this.sliderItems.length - i}`);
+          this.renderer.setStyle(rightSide, 'opacity', `${1 - (2 * i) / this.sliderItems.length}`);
+      }
     }
+  }
+
+  prev(): void {
+    this.activeOrder = Math.max(0, this.activeOrder - 1);
+    this.updateSlider();
+  }
+
+  next(): void {
+    this.activeOrder = Math.min(this.sliderItems.length - 1, this.activeOrder + 1);
+    this.updateSlider();
   }
 }
